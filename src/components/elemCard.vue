10349c21052a5c52a47e4d9ce18236e9034a2a9c
AstralGreen2 <Belov.vlad2002@yandex.ru>
2020-01-12 20:26:27 +0300

    Add files via upload
    reyhfcg

---
+++ src/components/elemCard.vue
@@ -0,0 +1,13 @@
+<template>
+<v-card>
+              <v-card-title>etretet</v-card-title>
+              <v-card-text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis, nam. Accusantium error facilis aperiam autem officia a assumenda earum reiciendis eos numquam, laboriosam, sint expedita necessitatibus dolorem, ratione excepturi ab.</v-card-text>
+            </v-card>
+</template>
+
+<script>
+export default{
+    name: 'Card'
+}
+    
+</script>
